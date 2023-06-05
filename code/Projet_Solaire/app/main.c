60b419f26e5e069d7237c022a16d8c09cea8f83c
gregoiremahon <greg.mahondu78@gmail.com>
2023-06-05 20:26:27 +0200

    Modifications main.c

--- code/Projet_Solaire/app/main.c
+++ code/Projet_Solaire/app/main.c
@@ -1,135 +1,115 @@
+# @author : Grégoire MAHON
+# EI2I 3 - II
+
 #include "stm32f0xx.h"
-
 #include "bsp.h"
-
 #include "delay.h"
-
 #include "main.h"
-
 #include <math.h>
 
-#define false 0
+#define FALSE 0
+#define TRUE 1
 
-#define true 1
-
+// Function Prototypes
 static void SystemClock_Config(void);
+static void Initialize_Variables(void);
+static uint16_t Read_PhotoResistor(uint8_t channel);
+static void Adjust_ServoAngles(void);
+
+// Global Variables
+uint16_t average_light_intensity;
+uint16_t diff_light_intensity_1_2;
+uint16_t diff_light_intensity_12_3;
+uint16_t light_intensity_1;
+uint16_t light_intensity_2;
+uint16_t light_intensity_3;
+uint16_t tolerance_horizontal;
+uint16_t tolerance_vertical;
+uint8_t horizon;
+uint8_t vertical;
+uint16_t servo_angle_1;
+uint16_t servo_angle_2;
 
 int main() {
-
-
-
-	// Declaration Variables
-
-	uint16_t moyenne;
-	uint16_t diff_photo1_2;
-	uint16_t diff_photo12_3;
-	uint16_t photo1;
-	uint16_t photo2;
-	uint16_t photo3;
-	uint16_t tolerance1;
-	uint16_t tolerance2;
-	uint8_t horizon;
-	uint8_t vertical;
-
-	uint16_t angle_servo1;
-	uint16_t angle_servo2;
-
-
-
     // Configure System Clock
     BSP_Console_Init();
-		mon_printf("La Console est ready!\r\n");
+    mon_printf("Console is ready!\r\n");
+
     // Initialize LED pin
     BSP_LED_Init();
-	  
     BSP_TIMER_PWM_Init();
-	
-		BSP_ADC_Init();
-	  mon_printf("ADC ready!\r\n");
+    BSP_ADC_Init();
+    mon_printf("ADC ready!\r\n");
+
+    // Initialize state variables
+    Initialize_Variables();
+
+    while(1){
+        light_intensity_1 = Read_PhotoResistor(10);
+        light_intensity_2 = Read_PhotoResistor(11);
+        light_intensity_3 = Read_PhotoResistor(12);
+        Adjust_ServoAngles();
+        BSP_DELAY_ms(1);
+    }
+}
+
+static void Initialize_Variables(void) {
+    servo_angle_1 = 2100;
+    servo_angle_2 = 1200;
+    tolerance_horizontal = 100; // 100 both
+    tolerance_vertical = 100;
+}
+
+static uint16_t Read_PhotoResistor(uint8_t channel) {
+    uint16_t light_intensity;
+    ADC1->CHSELR = ADC_CHSELR_CHSEL10 + channel;
+    ADC1->CR |= (1<<2); // Start conversion
+    while((ADC1->ISR & (1<<2)) != (1<<2)); // Wait for conversion to finish
+    light_intensity = (ADC1->DR)/1.23;
+    return light_intensity;
+}
+
+static void Adjust_ServoAngles(void) {
+    TIM1->CCR1 = servo_angle_1;
+    TIM1->CCR2 = servo_angle_2;
+
+    // Compute average light intensity
+    average_light_intensity = (light_intensity_1 + light_intensity_2) / 2;
+
+    // Adjust horizontal servo angle
+    if (light_intensity_1 < light_intensity_2){
+        diff_light_intensity_1_2 = abs(light_intensity_2 - light_intensity_1);
+        if(diff_light_intensity_1_2 > tolerance_horizontal){
+            if (servo_angle_2 < 2400){
+                servo_angle_2++;
+            }
+        }
+    }
+    if (light_intensity_2 < light_intensity_1){
+        diff_light_intensity_1_2 = abs(light_intensity_1 - light_intensity_2);
+        if(diff_light_intensity_1_2 > tolerance_horizontal){
+            if (servo_angle_2 > 600){
+                servo_angle_2--;
+            }
+        }
+    }
+
+    // Adjust vertical servo angle
+    if(average_light_intensity < light_intensity_3){
+        diff_light_intensity_12_3 = abs(average_light_intensity - light_intensity_3);
+        if(diff_light_intensity_12_3 > tolerance_vertical){
+            if(servo_angle_1 < 3000){
+                servo_angle_1++;
+            }
+        }
+    }
     
-    
-
-
-
-	//Initialize state variables
-
-	angle_servo1 = 2100;
-	angle_servo2 = 1200;
-	tolerance1 = 100; // 100 both
-	tolerance2 = 100;
-
-
-
-	while(1){
-		TIM1->CCR1 = angle_servo1;
-		TIM1->CCR2 = angle_servo2;
-		// Canal 10 (PC2)
-		ADC1->CHSELR = ADC_CHSELR_CHSEL10;
-    ADC1->CR |= (1<<2); // Démarrer la conversion
-    while((ADC1->ISR & (1<<2)) != (1<<2)); // Attente de la fin de conversion
-		photo1 = (ADC1->DR)/1.23;
-		// Canal 11 (PC0)
-		ADC1->CHSELR = ADC_CHSELR_CHSEL11;
-		ADC1->CR |= (1<<2);
-		while ((ADC1->ISR & (1<<2)) != (1<<2));
-		photo2 = (ADC1->DR)/1.23;
-		// Canal 12 (PC2)
-		ADC1->CHSELR = ADC_CHSELR_CHSEL12;
-		ADC1->CR |= (1<<2);
-		while ((ADC1->ISR & (1<<2)) != (1<<2));
-		photo3 = (ADC1->DR)/1.23;
-
-		//TRAITEMENT VALEUR
-		moyenne = (photo1 + photo2) / 2;
-
-		// Gestion de l'angle PWM
-		// Horizon
-
-		if (photo1 < photo2){
-			diff_photo1_2 = abs(photo2-photo1);
-			if(diff_photo1_2 > tolerance1){
-				if (angle_servo2 < 2400){
-					angle_servo2++;
-				}
-			}
-		}
-		if (photo2 < photo1){
-			diff_photo1_2 = abs(photo1-photo2);
-			if(diff_photo1_2 > tolerance1){
-				if (angle_servo2 > 600){ // 600
-					angle_servo2--;
-				}
-			}
-		}
-
-		// Vertical
-
-		if(moyenne<photo3){
-			diff_photo12_3 = abs(moyenne-photo3);
-			if(diff_photo12_3 > tolerance2){
-				if(angle_servo1 < 3000){ // 2400
-					angle_servo1++;
-				}
-			}
-		}
-		if(moyenne>photo3){
-			diff_photo12_3 = abs(photo3-moyenne);
-			if(diff_photo12_3 > tolerance2){
-				if(angle_servo1 > 1000){ // 2000
-					angle_servo1--;
-				}
-			}
-		}
-
-		// Affichage des valeurs via la liaison série
-		/*
-		mon_printf("Valeur photo resistance 1 :  = %d\r\n", photo1);
-		mon_printf("Valeur photo resistance 2 :  = %d\r\n", photo2);
-		mon_printf("Valeur photo resistance 3 :  = %d\r\n\n", photo3);
-		mon_printf("Valeur photo moyenne 12 :  = %d\r\n", moyenne);
-		mon_printf("Valeur photo angle1 :  = %d\r\n", angle_servo1);
-		mon_printf("Valeur photo angle2 :  = %d\r\n\n\n\n\n", angle_servo2);*/
-
-		BSP_DELAY_ms(1);
-	}
+    if(average_light_intensity > light_intensity_3){
+        diff_light_intensity_12_3 = abs(light_intensity_3 - average_light_intensity);
+        if(diff_light_intensity_12_3 > tolerance_vertical){
+            if(servo_angle_1 > 1000){
+                servo_angle_1--;
+            }
+        }
+    }
 }
--- code/Projet_Solaire/app/main.h
+++ code/Projet_Solaire/app/main.h
@@ -6,15 +6,25 @@
  */
 
 #include "stm32f0xx.h"
-
+#ifndef MAIN_H
+#define MAIN_H
 #ifndef APP_INC_MAIN_H_
 #define APP_INC_MAIN_H_
 
 /*
- * printf() et sprintf() de la lib. printf-stdarg.c
+ * printf() et sprintf() de la lib. printf-stdarg.c (@author : M.Darga)
  */
+
+
+// Function Prototypes
+static void SystemClock_Config(void);
+static void Initialize_Variables(void);
+static uint16_t Read_PhotoResistor(uint8_t channel);
+static void Adjust_ServoAngles(void);
+
+
 
 int mon_printf  (const char *format, ...);
 int mon_sprintf (char *out, const char *format, ...);
 
-#endif /* APP_INC_MAIN_H_ */
+#endif // MAIN_H
